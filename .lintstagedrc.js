4ade061d7f7f9577e6e6d25257205db1265c2da2
dodyvirgiawan <dody.virgiawan97@gmail.com>
2024-07-05 19:55:10 +0700

    Add notes to lintstaged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,7 @@
+// ? Note: I'm using lint-staged with husky and next lint as a pre-commit hook to prevent code that
+// ?       does not obey oureslint standard from being committed.
+// ? Reference: https://nextjs.org/docs/pages/building-your-application/configuring/eslint#lint-staged
+
 const path = require('path')
  
 const buildEslintCommand = (filenames) =>
